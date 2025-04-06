94e617e53e3d2487d1d444ed1b54cce7fe492a78
Aman Kumar <thisisamank@gmail.com>
2025-04-06 15:30:24 +0530

    feat: add basic structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+import starlightThemeObsidian from 'starlight-theme-obsidian';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			plugins: [starlightThemeObsidian()],
+			title: 'WAGMI playbook',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Roadmap',
+					autogenerate: { directory: 'roadmap' },
 				},
 			],
 		}),
