0a07af15c93bf9279022e464be17727477f49c83
Aman Kumar <thisisamank@gmail.com>
2025-04-06 15:42:14 +0530

    feat: CF pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,24 @@ import starlight from '@astrojs/starlight';
 import { defineConfig } from 'astro/config';
 import starlightThemeObsidian from 'starlight-theme-obsidian';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightThemeObsidian()],
-			title: 'WAGMI playbook',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Roadmap',
-					autogenerate: { directory: 'roadmap' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          plugins: [starlightThemeObsidian()],
+          title: 'WAGMI playbook',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: 'Roadmap',
+                  autogenerate: { directory: 'roadmap' },
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
